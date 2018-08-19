a254fba062d53663dcbf21af6e1b4febdbb36c5c
tpham20908 <tpham20908@gmail.com>
2018-08-18 22:37:33 -0400

    removed warning TextView

--- Practices/6RTest/app/src/main/java/ca/ipd12/tung/a6rtest/StartingScreenActivity.java
+++ Practices/6RTest/app/src/main/java/ca/ipd12/tung/a6rtest/StartingScreenActivity.java
@@ -18,13 +18,10 @@ import java.util.Map;
 import java.util.regex.Pattern;
 
 public class StartingScreenActivity extends MutualMenu {
-    TextView warning;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_starting_screen);
-        warning = findViewById(R.id.warning);
     }
 
     public void startingTest(View view) {
